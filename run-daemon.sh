7f7c8b0a45d141d32ca86530fb8b308752785820
mickmetalholic <yuanshenmick@gmail.com>
2018-05-24 11:22:00 +0800

    chore: stylesheet and echo information

--- run-daemon.sh
+++ run-daemon.sh
@@ -8,18 +8,22 @@ GISDATAURI=http://download.geofabrik.de/asia/china-latest.osm.pbf
 DAEMONNAME=osm-server
 
 prebuild() {
+  echo "Start doing some preperations..."
   # make directories
   mkdir -p $PGSQLDIR $GISDATADIR $RENDERCACHEDIR
   # download gis data
-  wget -P $GISDATADIR -O data.pbf $GISDATAURI
+  wget -P ${basepath}${GISDATADIR} -O data.pbf $GISDATAURI
   # download and process stylesheet
   wget https://github.com/gravitystorm/openstreetmap-carto/archive/v2.29.1.tar.gz
   tar -xzf v2.29.1.tar.gz
+  rm v2.29.1.tar.gz
   ./openstreetmap-carto-2.29.1/get-shapefiles.sh
+  echo "Preperations completed, now you can build the daemon."
 }
 
 # build docker daemon
 build() {
+  echo "Starting build the daemon "${DAEMONNAME}"..."
   docker build -t . $DAEMONNAME
 }
 
